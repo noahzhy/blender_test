92380b5a6d2a38b9d693fa00ad66bba994537a1f
noahzhy <noahzhy@gmail.com>
2023-03-13 06:26:02 +0900

    add nearest test method

--- test.py
+++ test.py
@@ -17,6 +17,11 @@ empty = bpy.data.objects["Empty"]
 cube = bpy.data.objects["Cube"]
 
 
+# function to calculate distance between two vectors
+def distance(v1, v2):
+    return (v1 - v2).length
+
+
 # function to get bone position
 def get_bone_pos_global(armature, bone_name):
     bone = armature.pose.bones[bone_name]
@@ -35,7 +40,7 @@ def occlusion_ray(scene, dg, origin, direction, distance=1000):
     empty.location = loc
     if is_hit:
         print("hit index: {}".format(index))
-        return is_hit, hit_obj.name_full, index
+        return is_hit, hit_obj.name_full, index, loc
     else:
         return is_hit, None, None
 
@@ -43,19 +48,21 @@ def occlusion_ray(scene, dg, origin, direction, distance=1000):
 def get_nearest_bvh(obj, vector):
     # find nearest bvh
     nearest_bvh = None
+
+    dg = bpy.context.evaluated_depsgraph_get()
     # get bvh tree of given object
-    bvh = BVHTree.FromObject(obj, bpy.context.evaluated_depsgraph_get())
-    # get nearest bvh
-    nearest_bvh = bvh.find_nearest_range(vector, 1.3)
-    print(nearest_bvh)
 
-    # # enter edit mode
-    # bpy.ops.object.mode_set(mode='EDIT')
-
-    # load bmesh from bvh
+    mesh = obj.evaluated_get(dg).to_mesh()
     bm = bmesh.new()
-    bm.from_mesh(obj.data)
-    # select nearest bvh
+    bm.from_mesh(mesh)
+    bm.transform(obj.matrix_world)
+    bvh = BVHTree.FromBMesh(bm)
+
+    # get nearest bvh
+    nearest_bvh = bvh.find_nearest_range(vector, .1)
+    # select nearest faces
+    for face in nearest_bvh:
+        obj.data.polygons[face[2]].select = True
 
     for face in nearest_bvh:
         bm.faces.ensure_lookup_table()
@@ -63,13 +70,23 @@ def get_nearest_bvh(obj, vector):
 
     # get all items[2]
     nearest_bvh = [item[2] for item in nearest_bvh]
-    # select
-    # sort by index
-    nearest_bvh.sort()
+    # sort by distance
+    nearest_bvh.sort(key=lambda x: distance(vector, bm.faces[x].calc_center_median()))
+
+    # new the cubes to the face center
+    for face in nearest_bvh:
+        bm.faces.ensure_lookup_table()
+        bm.faces[face].select = True
+        center = bm.faces[face].calc_center_median()
+        bpy.ops.mesh.primitive_cube_add(size=0.01, location=center)
+
     return nearest_bvh
 
 
-def is_occluded(camera, boneVec, threshold=0.01):
+c2b_idx = -1
+
+def is_occluded(camera, boneVec, threshold=1):
+    global c2b_idx
     # get current scene
     scene = bpy.context.scene
     dg = bpy.context.evaluated_depsgraph_get()
@@ -87,6 +104,12 @@ def is_occluded(camera, boneVec, threshold=0.01):
     c2b = occlusion_ray(scene, dg, camera.matrix_world.translation, pixel_vector, 100)
 
     if c2b[0] and b2c[0]:
+        c2b_idx = c2b[-2]
+        # the difference between b2c and c2b
+        diff = distance(b2c[-1], c2b[-1])
+        diff = abs(c2b[-2] - b2c[-2]) / diff
+        print("diff: {}".format(diff))
+        
         return (c2b != b2c)
     else:
         return True
@@ -97,11 +120,17 @@ if __name__ == '__main__':
     # get bone position in world space
     b_name = "forearm.L"
     boneVec = get_bone_pos_global(armature, b_name)
-    print(get_nearest_bvh(cube, boneVec))
+    n_idx = get_nearest_bvh(cube, boneVec)
+    print("nearest bvh: {}".format(n_idx))
 
     if is_occluded(camera, boneVec):
         print("{} occluded".format(b_name))
     else:
         print("{} not occluded".format(b_name))
 
+    if c2b_idx in n_idx:
+        print("{} in nearest".format(b_name))
+    else:
+        print("{} not in nearest".format(b_name))
 
+    print("c2b_idx: {}".format(c2b_idx))
