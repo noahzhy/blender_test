a79b5e493108aac5be0fb7437c0ae0f3b161ca0e
noahzhy <noahzhy@gmail.com>
2023-04-17 23:08:37 +0900

    add test

--- toolbar.py
+++ toolbar.py
@@ -1,37 +1,98 @@
+import time
+import math
+import random
+
 import bpy
 from bpy.props import *
 
 
+# custom operator
+class CustomOperator(bpy.types.Operator):
+    bl_idname = 'opr.custom_operator'
+    bl_label = 'Render'
+
+    def execute(self, context):
+        time_start = time.time()
+
+        # gen 10000 random numbers
+        for i in range(10000):
+            # caclulate cosin and sin
+            cos = math.cos(i)
+            sin = math.sin(i)
+            
+        # report time cost
+        time_cost = time.time() - time_start
+        self.report({'INFO'}, 'Time cost: {:.2f} seconds'.format(time_cost))
+        return {'FINISHED'}
+
+
 class PreviewPanel(bpy.types.Panel):
-    bl_idname = 'preview_panel'
+    bl_idname = 'VIEW3D_PT_preview_panel'
     bl_label = 'Preview'
     bl_space_type = 'VIEW_3D'
     bl_region_type = 'UI'
 
     def draw(self, context):
         col = self.layout.column()
-        for (prop_name, _) in PROPS:
-            row = col.row()
-            row.prop(context.scene, prop_name)
+
+        col.label(text="Focus On")
+        for (prop_name, _) in FOCUS_PROPS:
+            col.row().prop(context.scene, prop_name)
+
+        # divider
+        col.separator()
+        col.label(text="Camera")
+
+        for (prop_name, _) in CAM_PROPS:
+            col.row().prop(context.scene, prop_name)
 
         # button to render
         col.operator('render.render')
 
+        # divider
+        col.separator()
+        col.label(text="Batch Render")
+
+        for (prop_name, _) in BATCH_PROPS:
+            col.row().prop(context.scene, prop_name)
+
+        # custom button
+        col.operator('opr.custom_operator')
+
 
 CLASSES = [
     PreviewPanel,
+    CustomOperator,
 ]
 
-PROPS = [
-    ('prefix', bpy.props.StringProperty(name='Prefix', default='Pref')),
-    ('suffix', bpy.props.StringProperty(name='Suffix', default='Suff')),
-    ('add_version', bpy.props.BoolProperty(name='Add Version', default=False)),
-    ('version', bpy.props.IntProperty(name='Version', default=1)),
+FOCUS_PROPS = [
+    ('focus on', bpy.props.EnumProperty(
+        name='Bones',
+        items=[
+            ('camera', 'Camera', ''),
+            ('object', 'Object', ''),
+            ('point', 'Point', ''),
+            ('random', 'Random', ''),
+        ],
+        default='random',
+    )),
 ]
+
+CAM_PROPS = [
+    ('min scope', bpy.props.FloatProperty(name='Min Distance', default=0.1, min=0.1, max=10.0)),
+    ('max scope', bpy.props.FloatProperty(name='Max Distance', default=1.0, min=0.1, max=10.0)),
+]
+
+BATCH_PROPS = [
+    ('amount', bpy.props.IntProperty(name='Amount', default=10, min=1)),
+]
+
+TOTAL_PROPS = FOCUS_PROPS + CAM_PROPS + BATCH_PROPS
 
 
 def register():
-    for (prop_name, prop_value) in PROPS:
+    # stack PROPS and BATCH_PROPS
+    for (prop_name, prop_value) in TOTAL_PROPS:
         setattr(bpy.types.Scene, prop_name, prop_value)
     
     for klass in CLASSES:
@@ -39,7 +100,7 @@ def register():
 
 
 def unregister():
-    for (prop_name, _) in PROPS:
+    for (prop_name, _) in TOTAL_PROPS:
         delattr(bpy.types.Scene, prop_name)
 
     for klass in CLASSES:
@@ -48,3 +109,4 @@ def unregister():
 
 if __name__ == '__main__':
     register()
+    # unregister()
