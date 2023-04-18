ff89817910624b396436926f1d52ad1f3e4cdd8f
noahzhy <noahzhy@gmail.com>
2023-04-18 11:44:01 +0900

    fix palette bug

--- gen_palette.py
+++ gen_palette.py
@@ -1,20 +1,25 @@
 import numpy as np
 import matplotlib
 import matplotlib.pyplot as plt
-from PIL import Image, ImageDraw
+from PIL import Image, ImageDraw, ImageColor
 
 import os
 import sys
 import glob
+import math
 import random
 
 # generate 24 color palette in hsv
 def gen_palette(n=16):
     # generate 24 colors in order of hue with PIL
-    img = Image.new('HSV', (1, n), (0, 0, 0))
+    img = Image.new('RGB', (1, n), (0, 0, 0))
     draw = ImageDraw.Draw(img)
     for i in range(n):
-        draw.point((0, i), (int(i * (256/n)), 255, 255))
+        # draw.point((0, i), (int(255 * i / n), 255, 255))
+        # hsv to rgb
+        rgb = ImageColor.getrgb("hsv(%d, 100%%, 100%%)" % (int(360 * i / n)))
+        draw.point((0, i), rgb)
+
     del draw
     # convert to rgb
     img = img.convert('RGB')
